00137e849e260f00d6832949ec5b59997eeaa049
smitranjansingh29 <62330908+smitranjansingh29@users.noreply.github.com>
2025-11-22 12:21:52 +0530

    Remove experimental server components configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@neondatabase/serverless'],
-  },
+  serverExternalPackages: ['@neondatabase/serverless'],
 };
 
 module.exports = nextConfig;
